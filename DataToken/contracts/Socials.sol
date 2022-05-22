e6a3fea27b12481fb55885a611248f0c68b08b75
defiliquiditylabs <104155335+defiliquiditylabs@users.noreply.github.com>
2022-05-22 03:58:09 +0100

    fixed bugs

--- DataToken/contracts/Socials.sol
+++ DataToken/contracts/Socials.sol
@@ -1,55 +1,78 @@
 pragma solidity 0.8.0;
-import "./interfaces/INFT.sol";
 import "./interfaces/IERCReceiver.sol";
+import "./ERC.sol";
 import "./Dependencies/Ownable.sol";
 
 
 
-contract socialMedia is Ownable{
+contract socialMedia is Ownable, ERC{
 
-    INFT public immutable  Profile;
     uint postId;
+    uint commentId;
 
     struct POSTS{
-        uint id;
-        bytes post;
+        address Poster;
+        uint Id;
+        string post;
+    }
+
+    struct COMMENTS{
+        address Commenter;
+        uint Id;
+        string comment;
     }
 
     POSTS [] public allPost;
+    COMMENTS [] public allComment;
 
-    constructor (INFT _Profile){
-        Profile=_Profile;
+    constructor ()ERC("socialMedia", "SMA"){
+        
     }
     
-    event profileCreated(bytes tokenId, address owner, string name, uint time);
-    event ProfileUpdated(bytes tokenId, address owner, string name, uint time);
-    event BioCreated(string bio, address owner, uint time );
-    event updateBio(string Newbio, address owner, uint time );
-    event postCreated (address sender, bytes post, uint256 time, uint postId);
-    event commented(uint index, bytes post);
-    event postDeleted();
+    event profileCreated(bytes indexed tokenId, address indexed owner, string indexed name, uint time);
+    event ProfileUpdated(bytes indexed tokenId, address indexed owner, string indexed name, uint time);
+    event BioCreated(string indexed bio, address indexed owner, uint time );
+    event updateBio(string indexed Newbio, address indexed owner, uint time );
+    event postCreated (address indexed sender, string indexed post, uint256 time, uint indexed postId);
+    event postDeleted(bytes indexed tokenId, uint indexed index);
+    event commented(address indexed postedBy, address indexed commentBy, string indexed commentedOn, string indexedcomment, uint commentId, uint time);
     
 
-    function createPost(string calldata post) external returns (bytes memory){
+    function createPost(string calldata post) external{
         uint time=block.timestamp;
         address sender=msg.sender;
-        bytes memory Post=abi.encode(post);
+        //bytes memory Post=abi.encode(post);
         postId++;
-        allPost.push(POSTS(postId, Post));
-        emit postCreated (sender, Post, time, postId);
-        return Post;
-         
+        allPost.push(POSTS(sender,postId, post));
+        emit postCreated (sender, post, time, postId);   
+
+    }
+
+    /*function createPost(string calldata post) external {
+        postId++;
+        address to=msg.sender;
+        bytes memory newtokenId=(abi.encode(post));
+        _safeMint(to, newtokenId);
+        allPost.push(POSTS(postId, newtokenId));
+    }*/
+
+    function commentOnPost(uint index, string calldata comment)public{
+        uint time=block.timestamp;
+        address commentBy=msg.sender;
+        commentId++;
+        POSTS storage _post=allPost[index];
+        string memory commentedOn= _post.post;
+        address postedBy= _post.Poster;
+        allComment.push(COMMENTS(commentBy, commentId, comment));
+        emit commented(postedBy, commentBy, commentedOn, comment, commentId, time);
         
 
     }
 
-    
-
-
-
     function deletePost(bytes calldata tokenId, uint index)external{
-        Profile.burn(tokenId);
+        _burn(tokenId);
         delete allPost[index];
+        emit postDeleted(tokenId, index);
     }
 
     function decodePost(bytes memory data) public pure returns (string memory post) {
@@ -58,31 +81,45 @@ contract socialMedia is Ownable{
 
 
     function mintAprofile(string calldata name, address to)public{
-        Profile.safeMint(name, to);
+        uint time=block.timestamp;
+        bytes memory tokenId=(abi.encode(name, to,  time));
+        _safeMint(to, tokenId);
+        emit profileCreated(tokenId, to, name, time);
+        
 
     }
 
-    function UpdateAprofile(bytes calldata oldTokenId, string calldata Newname, address to) onlyOwner public{
-        Profile.burn(oldTokenId);
-        Profile.safeMint(Newname, to);
+    function UpdateAprofile(bytes calldata oldTokenId, string calldata newName, address to) onlyOwner public{
+        _burn(oldTokenId);
+        uint time=block.timestamp;
+        bytes memory newTokenId=(abi.encode(oldTokenId, newName, to, time));
+        _safeMint(to, newTokenId );
+        emit ProfileUpdated(newTokenId, to, newName, time);
     }
 
     function mintABio(string calldata bio, address to) public{
         uint time=block.timestamp;
-        Profile.safeMint(bio, to);
+        bytes memory tokenId=(abi.encode(bio, to, time));
+        _safeMint(to, tokenId);
         emit BioCreated( bio, to, time ); 
     }
 
-    function UpdateABio(bytes calldata oldTokenId, string calldata NewBio, address to) onlyOwner public{
-       uint time=block.timestamp;
-        Profile.burn(oldTokenId);
-        Profile.safeMint(NewBio, to);
-        emit updateBio( NewBio, to,  time );
+    function UpdateABio(bytes calldata oldTokenId, string calldata newBio, address to) onlyOwner public{
+       _burn(oldTokenId);
+        uint time=block.timestamp;
+        bytes memory newTokenId=(abi.encode(oldTokenId, newBio, to, time));
+        _safeMint(to, newTokenId );
+        emit updateBio( newBio, to,  time );
     }
 
-    function getPost(uint index) public view returns (uint, bytes memory) {
+    function getPost(uint index) public view returns (address, uint, string memory) {
         POSTS storage _post=allPost[index];
-        return (_post.id, _post.post);
+        return (_post.Poster, _post.Id, _post.post);
+    }
+
+    function getcomment(uint index) public view returns (address, uint, string memory) {
+        COMMENTS storage _comment=allComment[index];
+        return (_comment.Commenter, _comment.Id, _comment.comment);
     }
 
     
@@ -91,7 +128,7 @@ contract socialMedia is Ownable{
     }
 
 
-    function onERCReceived(
+    function onERC721Received(
         address,
         address from,
         bytes calldata,
